34645fb3f18b33daa678f9cdc4df7a1af203a537
juan <juan.antoniorr@hotmail.com>
2020-08-05 19:41:06 -0500

    Adding eureka dependency and removing ribbon

--- src/main/java/com/proyecto/springboot/items/SpringbootServicioItemsApplication.java
+++ src/main/java/com/proyecto/springboot/items/SpringbootServicioItemsApplication.java
@@ -2,10 +2,12 @@ package com.proyecto.springboot.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "servicio-productos") //Permite usar el balanceador Ribbon
+@EnableEurekaClient
+//@RibbonClient(name = "servicio-productos") //Permite usar el balanceador Ribbon
 @EnableFeignClients //Permite usar clientes Feign e inyectarlos
 @SpringBootApplication
 public class SpringbootServicioItemsApplication {
