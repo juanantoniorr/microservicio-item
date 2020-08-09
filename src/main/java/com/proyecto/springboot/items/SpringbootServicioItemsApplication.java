50ff6f7657cc34c8306306a014188d80f2cac999
juan <juan.antoniorr@hotmail.com>
2020-08-09 17:59:38 -0500

    Agregando tolerancia a fallos con hystrix

--- src/main/java/com/proyecto/springboot/items/SpringbootServicioItemsApplication.java
+++ src/main/java/com/proyecto/springboot/items/SpringbootServicioItemsApplication.java
@@ -2,10 +2,11 @@ package com.proyecto.springboot.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 //@RibbonClient(name = "servicio-productos") //Permite usar el balanceador Ribbon
 @EnableFeignClients //Permite usar clientes Feign e inyectarlos
--- src/main/java/com/proyecto/springboot/items/controllers/ItemController.java
+++ src/main/java/com/proyecto/springboot/items/controllers/ItemController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.proyecto.springboot.items.models.Item;
+import com.proyecto.springboot.items.models.Producto;
 import com.proyecto.springboot.items.models.service.ItemService;
 
 @RestController
@@ -24,11 +26,21 @@ public class ItemController {
 		
 	}
 	
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle (@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
-		
-		
+	}
+	
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Nombre por defecto");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		return item;
 	}
 
 }
