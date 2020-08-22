49e30db2f5abc9796109c11f921f6702eeab1fff
juan <juan.antoniorr@hotmail.com>
2020-08-22 18:34:21 -0500

    Adding @refreshScope and Actuator to refresh properties

--- src/main/java/com/proyecto/springboot/items/controllers/ItemController.java
+++ src/main/java/com/proyecto/springboot/items/controllers/ItemController.java
@@ -1,9 +1,16 @@
 package com.proyecto.springboot.items.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +19,17 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.proyecto.springboot.items.models.Item;
 import com.proyecto.springboot.items.models.Producto;
 import com.proyecto.springboot.items.models.service.ItemService;
-
+@RefreshScope
 @RestController
 public class ItemController {
+	//Inyectando este bean puedes llamar propiedades y perfiles activos
+	@Autowired
+	private Environment env;
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List <Item> listar(){
@@ -26,6 +38,16 @@ public class ItemController {
 		
 	}
 	
+	@GetMapping("obtener-config")
+	public ResponseEntity<?> obtenerConfiguracion(@Value("${server.port}") String puerto) {
+		Map<String,String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		if (env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("Autor", env.getProperty("autor.nombre"));
+		}
+		return new ResponseEntity<Map<String,String>> (json,HttpStatus.OK);
+	}
 	//Con este comando si hay un fallo llamara al otro metodo
 	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
