4152ecc963ee5323532acbb20287da9e02a1f5c8
juan <juan.antoniorr@hotmail.com>
2020-09-10 18:14:23 -0500

    Adding post,put,delete in controller and change qualifier to restTemplate

--- src/main/java/com/proyecto/springboot/items/controllers/ItemController.java
+++ src/main/java/com/proyecto/springboot/items/controllers/ItemController.java
@@ -3,7 +3,6 @@ package com.proyecto.springboot.items.controllers;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,8 +10,13 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -26,7 +30,7 @@ public class ItemController {
 	@Autowired
 	private Environment env;
 	@Autowired
-	@Qualifier("serviceFeign")
+	@Qualifier("serviceRest")
 	private ItemService itemService;
 	
 	
@@ -63,6 +67,22 @@ public class ItemController {
 		producto.setPrecio(500.00);
 		item.setProducto(producto);
 		return item;
+	}
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto crear (@RequestBody Producto producto) {
+		return itemService.save(producto);
+	}
+	
+	@PutMapping("/editar/{id}")
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
+		return itemService.update(producto, id);
+		
+	}
+	@DeleteMapping("/eliminar/{id}")
+	public void eliminar(@PathVariable Long id) {
+		itemService.delete(id);
+		
 	}
 
 }
